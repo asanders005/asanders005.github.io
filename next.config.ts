76ab6d94b794782c424f2c2b598bd9c75fa68d05
Aiden Sanders <aidensandersbiz@gmail.com>
2026-04-08 10:33:29 -0600

    Remove basePath for testing deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/public",
   reactStrictMode: true,
   images: {
     unoptimized: true,
